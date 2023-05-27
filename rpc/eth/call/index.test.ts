8c59ac2e101805ea7e6edb8dced00a32631ed7ed
Raby Yuson <ryuson@horizenlabs.io>
2023-05-27 01:21:31 -0400

    chore: rename label

--- rpc/eth/call/index.test.ts
+++ rpc/eth/call/index.test.ts
@@ -4,7 +4,7 @@ import evaluateResponse from "../../../utils/evaluateResponse";
 import patternGenerator from "../../../utils/patternGenerator";
 
 describe("eth_call", () => {
-  it("Executes a new message call immediately without creating a transaction on the block chain.", async () => {
+  it("Executes a new message call immediately without creating a transaction on the blockchain.", async () => {
     evaluateResponse({
       response: await eth_call(), 
       pattern: await patternGenerator.buildStringPattern({
