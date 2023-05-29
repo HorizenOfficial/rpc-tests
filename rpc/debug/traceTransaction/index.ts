02d202f1c85aed46fa091d9cb13fcead67061394
Raby Yuson <ryuson@horizenlabs.io>
2023-05-29 15:41:29 -0400

    feat: add debug rpc methods

--- rpc/debug/traceTransaction/index.ts
+++ rpc/debug/traceTransaction/index.ts
@@ -2,14 +2,14 @@ import fetchAPI from "../../../utils/fetchAPI";
 import { JSONRPC } from "../../../utils/types";
 import fixtures from "../../../fixtures";
 
-const eth_accounts = async ():
+const debug_traceTransaction = async ():
   Promise<JSONRPC> => await fetchAPI({
     options: {
       id: fixtures.id,
       jsonrpc: fixtures.jsonrpc,
-      method: "eth_accounts",
-      params: [],
+      method: "debug_traceTransaction",
+      params: ["0x5d094d1a3bcd65eb8955a66ef8554f106400779934aa6d94a68bd6043f19415d", {}],
     },
   });
 
-export default eth_accounts;
+export default debug_traceTransaction;
