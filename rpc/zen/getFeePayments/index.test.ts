c0a9789e85bb32aebde86b08d49da1ce1d1d4f6b
Yuriko Inaba <yurikoainaba@gmail.com>
2024-03-08 16:56:32 -0500

    Update zen_getFeePayment for gobi (#26)

--- rpc/zen/getFeePayments/index.test.ts
+++ rpc/zen/getFeePayments/index.test.ts
@@ -6,7 +6,7 @@ import patternGenerator from "../../../utils/patternGenerator";
 describe("zen_getFeePayments", () => {
   it("Returns the list of the forger rewards (recipients and values) distributed at the specified block.", async () => {
     // TODO:  1.3.0 returns empty array instead of null, update this each time we deploy to an env and remove once live on EON.
-    const expectNull = !(process.env.BLOCKSCOUT_API && process.env.BLOCKSCOUT_API.includes('pregobi'))
+    const expectNull = !(process.env.BLOCKSCOUT_API && process.env.BLOCKSCOUT_API.includes('gobi'))
     const schema = await patternGenerator.getSchema();
     evaluateResponse({
       response: await zen_getFeePayments(), 
